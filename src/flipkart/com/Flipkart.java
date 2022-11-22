428a8501187857040c21ae3fdbe6c707228fd3e5
SANDY281093 <santhos281093@gmail.com>
2022-11-22 18:24:39 +0530

    Master

---
+++ src/flipkart/com/Flipkart.java
@@ -0,0 +1,15 @@
+package flipkart.com;
+
+public class Flipkart {
+	public static void main(String[] args) {
+		int a=10,b=20;
+	b=b-a;
+	a=a+b;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(a+b);
+
+		
+	}
+
+}
